37dc6955dac34eeceabf222d4203296bb8dd7b45
Clémentine <clementine.robert.nantes@gmail.com>
2021-03-04 18:38:04 +0100

    all tp

---
+++ todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
@@ -0,0 +1,47 @@
+package com.acme.todolist.adapters.rest_api;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.acme.todolist.application.port.in.GetTodoItems;
+import com.acme.todolist.application.port.in.AddTodoItem;
+import com.acme.todolist.domain.TodoItem;
+
+/**
+ * Le controlleur Spring MVC qui expose les endpoints REST
+ * 
+ * @author bflorat
+ *
+ */
+@RestController
+public class TodoListController {
+	
+	
+	private GetTodoItems getTodoItemsQuery;
+	private AddTodoItem addTodoItem;
+	
+	
+	@Inject
+	public TodoListController(GetTodoItems getTodoItemsQuery, AddTodoItem addTodoItem ) {
+		this.getTodoItemsQuery = getTodoItemsQuery;
+		this.addTodoItem = addTodoItem;
+	}
+	
+	@GetMapping("/todos")
+	public List<TodoItem> getAllTodoItems() {
+		return this.getTodoItemsQuery.getAllTodoItems();
+	}
+	
+	@PostMapping("/todos")
+	public void ajouterItem(@RequestBody TodoItem item) {
+		this.addTodoItem.addTodoItem(item);
+	}
+	
+	
+}
